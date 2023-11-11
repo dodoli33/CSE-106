587c4d7201af90d2b3c6d068e8505522c15c162c
xGryffinclaw54x <116847423+xGryffinclaw54x@users.noreply.github.com>
2023-11-10 20:28:28 -0800

    Teacher (#2)
    * Teacher course dashboard
    
    * Student grade dashboard
    
    * Quality of life improvements to student_dashboard
    
    * Edit student grades
    
    ---------
    
    Co-authored-by: sparklyunico <riteshpatro14@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Sequence, and_
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_wtf import FlaskForm
@@ -25,15 +26,19 @@ student_course = db.Table(
 )
 
 class Student(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, Sequence('student_id_seq', start=100), primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
     enrolled_courses = db.relationship('Course', secondary=student_course, backref='students')
 
 class Teacher(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -43,7 +48,6 @@ class Course(db.Model):
     enrolled_students = db.Column(db.Integer, default=0)
     capacity = db.Column(db.Integer, nullable=False)
     #students_list = db.relationship('Student', secondary=student_course, backref='courses_enrolled')
-
 
 
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
@@ -81,7 +85,43 @@ def login():
 @app.route('/teacher_dashboard')
 @login_required
 def teacher_dashboard():
-    return "Welcome to the teacher dashboard, {}".format(current_user.username)
+    fullName = str(current_user.first_name) + " " + str(current_user.last_name)
+    courses = Course.query.filter_by(teacher_name=fullName).all()
+    return render_template('teacher.html', username=current_user.last_name, courses=courses)
+
+@app.route('/teacher_dashboard/<courseID>')
+@login_required
+def student_grade_dashboard(courseID):
+    fullName = str(current_user.first_name) + " " + str(current_user.last_name)
+    course = Course.query.filter_by(id=courseID).first()
+
+    if course.teacher_name == fullName:
+        students = db.session.query(Student.first_name, Student.last_name, student_course.c.grade).join(student_course).filter(student_course.c.course_id == courseID).all()
+
+        if students:
+            return render_template('studentGrades.html', course=course, students=students)
+        else:
+            return redirect(url_for('teacher_dashboard'))
+    else:
+        return redirect(url_for('teacher_dashboard'))
+    
+@app.route('/editGrades/<courseID>', methods=['PUT'])
+def edit_student_grades(courseID):
+    data = request.json 
+
+    for student_data in data:
+        first_name = student_data['first_name']
+        last_name = student_data['last_name']
+        grade = student_data['grade']
+            
+        # Update the grade in the database
+        student = Student.query.filter_by(first_name=first_name, last_name=last_name).first()
+        if student:
+            courseGrade = student_course.update().where(and_(student_course.c.student_id == student.id,student_course.c.course_id == courseID)).values(grade=grade)
+            db.session.execute(courseGrade)
+        
+    db.session.commit()
+    return jsonify({'message': 'Grades updated successfully'})
 
 def not_courses(student_courses):
     #print(student_courses + '\n', file=sys.stderr)
@@ -102,7 +142,7 @@ def student_dashboard_your_courses():
     student_courses = current_user.enrolled_courses #should give you all courses student is enrolled in
     all_courses = Course.query.filter(Course.enrolled_students < Course.capacity).all()
     not_student_courses = not_courses(student_courses) #should give you all courses student isn't enrolled in
-    return render_template('student.html', username=current_user.username, courses=student_courses, student_courses=student_courses, all_courses=all_courses, not_student_courses=not_student_courses)
+    return render_template('student.html', username=current_user.first_name, courses=student_courses, student_courses=student_courses, all_courses=all_courses, not_student_courses=not_student_courses)
 #render_template('student.html', username=current_user.username, courses=student_courses)
 
 
@@ -112,25 +152,26 @@ def student_dashboard_add_courses():
     all_courses = Course.query.filter(Course.enrolled_students < Course.capacity).all()
     student_courses = current_user.enrolled_courses #should give you all courses student is enrolled in
     not_student_courses = not_courses(student_courses) #should give you all courses student isn't enrolled in
-    return render_template('student_add.html', username=current_user.username, student_courses=student_courses, all_courses=all_courses, not_student_courses=not_student_courses)
+    return render_template('student_add.html', username=current_user.first_name, student_courses=student_courses, all_courses=all_courses, not_student_courses=not_student_courses)
 
+#This is reaching a reload of the student page
 @app.route('/reload_student_add_courses/<courseId>')
 @login_required
 def reload_student_add_courses(courseId):
-    print("This is reaching a reload\n")
+    #This has found a course by searching for course ID
     course = Course.query.filter(Course.id == courseId).first()
-    print("This has found a course by searching for course Id\nCourse Id: %d course name: %s", course.id, course.class_name)
     student = Student.query.filter(Student.id == current_user.id).first()
     adding = True
-    print("Student enrolled courses: ", student.enrolled_courses)
 
     for enrolled in student.enrolled_courses:
         if course == enrolled:
             student.enrolled_courses.remove(course)
+            course.enrolled_students = course.enrolled_students - 1
             adding = False
 
-    if adding:
+    if adding and not course.enrolled_students >= course.capacity:
         student.enrolled_courses.append(course)
+        course.enrolled_students = course.enrolled_students + 1
 
     db.session.commit()
 
@@ -176,4 +217,9 @@ def view_enrolled_courses():
 
 
 if __name__ == '__main__':
+    # with app.app_context():
+    #     student = Student(id='104', username='mnorris', password='111', first_name='Mindy', last_name='Norris')
+    #     db.session.add(student)
+    #     db.session.commit()
+
     app.run(port=5001)
